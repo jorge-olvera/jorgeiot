d997bdb3f80b44a3988cc41cf2f8f92f006621d2
jorge-olvera <martynez.jorge456@gmail.com>
2021-04-10 12:21:06 -0500

    cambie footer y puse el link a mi bd de firestore

--- cmp/mi-footer.js
+++ cmp/mi-footer.js
@@ -3,7 +3,7 @@ class MiFooter
   connectedCallback() {
     this.innerHTML = /* html */
       `&copy; 2021
-      Gilberto Pacheco Gallegos.`;
+      Jorge Olvera Martinez.`;
   }
 }
 customElements.define(
--- js/init.js
+++ js/init.js
@@ -1,10 +1,9 @@
 // @ts-ignore
 firebase.initializeApp({
-  apiKey: "AIzaSyDfJiJW2_AW1p-PKmt3w6h7kWPc9VmvzYM",
-  authDomain: "gilpgiotx.firebaseapp.com",
-  databaseURL: "https://gilpgiotx.firebaseio.com",
-  projectId: "gilpgiotx",
-  storageBucket: "gilpgiotx.appspot.com",
-  messagingSenderId: "362186415969",
-  appId: "1:362186415969:web:e732f8610cfdae9544ef20"
+  apiKey: "AIzaSyD68hVxjpa4BILsqXcJ3y4OyMUQbocjl1Y",
+  authDomain: "jorgeio-utn.firebaseapp.com",
+  projectId: "jorgeio-utn",
+  storageBucket: "jorgeio-utn.appspot.com",
+  messagingSenderId: "642162217324",
+  appId: "1:642162217324:web:9bb297adbfa14d7c89e349"
 });
